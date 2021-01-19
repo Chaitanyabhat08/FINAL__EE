c1a2855eb934e8785096619178277463656d506c
Chaitanyabhat08 <chaitanyabhat08@gmail.com>
2021-01-19 15:17:43 +0530

    first commit

---
+++ sele.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver=webdriver.Chrome(executable_path="C:\Driver\chromedriver.exe")
+driver.get("https://studentmanagementsystem8.herokuapp.com/admin_home")
+print(driver.title)
+driver.close()
